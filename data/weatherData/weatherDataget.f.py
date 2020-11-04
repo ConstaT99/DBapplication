5189355ed493583b12377ab91900c0fd1ade3ebb
Tao, Jerry <rtao6@illinois.edu>
2020-11-03 21:06:37 -0600

    weather data

---
+++ data/weatherData/weatherDataget.f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from wwo_hist import retrieve_hist_data
+import os
+import numpy as np
+os.chdir("./")
+frequency = 24
+start_date = '01-JAN-2013'
+end_date = '02-NOV-2020'
+api_key = '596f2032f396453b82b235521200211'
+My_list = []
+for i in range(90034, 96163):
+    My_list.append(str(i))
+# location_list = ['california']
+hist_weather_data = retrieve_hist_data(api_key, My_list, start_date, end_date, frequency, location_label = False, export_csv = True, store_df = True)
