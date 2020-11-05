b187bde7154f5c38587eaeee4355909b3f09da51
Tao, Jerry <rtao6@illinois.edu>
2020-11-05 17:58:17 -0600

    data modify

---
+++ data/fireData/proceedData.f.py
@@ -0,0 +1,4 @@
+import pandas as pd
+import numpy as np
+
+fire = pd.read_csv("WildFireData.csv")
--- data/weatherData/weatherDataget.f.py
+++ data/weatherData/weatherDataget.f.py
@@ -7,8 +7,6 @@ frequency = 24
 start_date = '01-JAN-2013'
 end_date = '02-NOV-2020'
 api_key = '596f2032f396453b82b235521200211'
-My_list = []
-for i in range(90034, 96163):
-    My_list.append(str(i))
+My_list = ["garden-grove","Glendale" ,"Hayward"," Huntington-Beach", "Irvine", "Lancaster","Long-Beach", "Los-Angeles"," Modesto ","MorenoValley","Oakland","Oceanside","Ontario","Orange","Oxnard","Palmdale","Pasadena", "Pomona", "Rancho-Cucamonga", "Riverside", "Sacramento", "Salinas", "San-Bernardino", "San-Diego", "San-Francisco", "San-Jose", "Santa-Ana", "Santa-Clarita", "Santa-Rosa", "Stockton", "Torrance"]
 # location_list = ['california']
 hist_weather_data = retrieve_hist_data(api_key, My_list, start_date, end_date, frequency, location_label = False, export_csv = True, store_df = True)
