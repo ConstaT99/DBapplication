81f5e7cc485c39cba438e5efb0288835e9d09687
Tao, Jerry <rtao6@illinois.edu>
2020-11-05 20:09:13 -0600

    weather data

--- data/weatherData/weatherDataget.f.py
+++ data/weatherData/weatherDataget.f.py
@@ -7,6 +7,6 @@ frequency = 24
 start_date = '01-JAN-2013'
 end_date = '02-NOV-2020'
 api_key = '596f2032f396453b82b235521200211'
-My_list = ["garden-grove","Glendale" ,"Hayward"," Huntington-Beach", "Irvine", "Lancaster","Long-Beach", "Los-Angeles"," Modesto ","MorenoValley","Oakland","Oceanside","Ontario","Orange","Oxnard","Palmdale","Pasadena", "Pomona", "Rancho-Cucamonga", "Riverside", "Sacramento", "Salinas", "San-Bernardino", "San-Diego", "San-Francisco", "San-Jose", "Santa-Ana", "Santa-Clarita", "Santa-Rosa", "Stockton", "Torrance"]
+My_list = ["modesto","moreno-valley","oakland","oceanside","ontario","orange","Oxnard","Palmdale","Pasadena", "Pomona", "rancho-cucamonga", "Riverside", "Sacramento", "Salinas", "san-bernardino", "san-diego", "san-francisco", "san-jose", "santa-ana", "santa-clarita", "santa-rosa", "Stockton", "Torrance"]
 # location_list = ['california']
 hist_weather_data = retrieve_hist_data(api_key, My_list, start_date, end_date, frequency, location_label = False, export_csv = True, store_df = True)
