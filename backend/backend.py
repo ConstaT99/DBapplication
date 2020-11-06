a5e0d84235db187e845e3c618fd698973ce360f0
Dian Zhang <zhan5999@umn.edu>
2020-11-05 22:10:31 -0600

    user create

--- backend/backend.py
+++ backend/backend.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request, jsonify
+from flask import request, jsonify, redirect
 from pymongo import MongoClient
 import numpy as np
 import datetime
@@ -28,40 +28,89 @@ books = [
      'first_sentence': 'to wound the autumnal city.',
      'published': '1975'}
 ]
+# userDocument = {
+#   "userId":  "404" ,
+#   "password": "12345",
+#   "nickName": "small4",
+#   "alertLocation": "SF",
+#   "phoneNumber": 12345678,
+#   "email" : "411411@gmail.com"
+# }
+
+fireDocument = {
+  "userId": { "404DontTrustTears" },
+  "password": {"areukiddingme"},
+  "nickName": {"small4"},
+  "alertLocation": {"SF"},
+  "phoneNumber": {1234567},
+  "email" : {"411411@gmail.com"}
+}
 
 client = MongoClient('localhost', 27017)
-db = client.test  # create test collection
+db = client.projectnull  # create test collection
+user = db.user
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='teamnull',
-                             db='db',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+# connection = pymysql.connect(host='localhost',
+#                              user='root',
+#                              password='teamnull',
+#                              db='db',
+#                              charset='utf8mb4',
+#                              cursorclass=pymysql.cursors.DictCursor)
+#
+#
+# cursor = connection.cursor()
+
+#
+# @app.route('/', methods=['GET'])
+# def home():
+#     return '''<h1>Distant Reading Archive</h1>
+# <p>A prototype API for distant reading of science fiction novels.</p>'''
+#
+#
+# # A route to return all of the available entries in our catalog.
+# @app.route('/api/people/mongodb', methods=['GET'])
+# def api_mongodb():
+#     return dumps(db.people.find_one())
 
 
-cursor = connection.cursor()
+# @app.route('/api/people/mysql', methods=['GET'])
+# def api_mysql():
+#     sql = "SELECT * FROM people WHERE name=%s"
+#     # use request.args.get to fetch get params
+#     # e.g. GET /api/people/mysql?name=root
+#     cursor.execute(sql, request.args.get('name'))
+#     return jsonify(cursor.fetchall())
 
+print("wwwwwww")
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
-
-
-# A route to return all of the available entries in our catalog.
 @app.route('/api/people/mongodb', methods=['GET'])
-def api_mongodb():
-    return dumps(db.people.find_one())
+def createUser():
+    print("aaa")
+    userId = request.args.get('userId')
+    password = request.args.get('password')
+    nickName = request.args.get('nickName')
+    alertLocation = request.args.get('alertLocation')
+    phoneNumber = request.args.get('phoneNumber')
+    email = request.args.get('email')
+    exist_check = user.find({"userId":userId},{"_id":0}).count()
 
+    if exist_check >=1:
+        print("ttttttttttttttt")
+        return dumps(print("Account already exsits")),400
+    else:
+        userDocument = {
+          "userId":userId,
+          "password":password,
+          "nickName":nickName,
+          "alertLocation": alertLocation,
+          "phoneNumber": alertLocation,
+          "email" : email
+        }
+        user.insert_one(userDocument)
+        print("Field value is not present")
+        return dumps(user.find({"userId":userId},{"password":0}))
+        # return redirect('http://localhost:3000')
 
-@app.route('/api/people/mysql', methods=['GET'])
-def api_mysql():
-    sql = "SELECT * FROM people WHERE name=%s"
-    # use request.args.get to fetch get params
-    # e.g. GET /api/people/mysql?name=root
-    cursor.execute(sql, request.args.get('name'))
-    return jsonify(cursor.fetchall())
 
 
 app.run()
---
+++ db/functions/user/createUser.f.py
@@ -0,0 +1,52 @@
+import flask
+from flask import request, jsonify
+from pymongo import MongoClient
+import numpy as np
+import datetime
+from bson.json_util import dumps
+import pymysql.cursors
+from flask import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+client = MongoClient('localhost', 27017)
+db = client.projectnull # create test collection
+user = db.user
+
+userDocument = {
+  "userId": { "404DontTrustTears" },
+  "password": {"areukiddingme"},
+  "nickName": {"small4"},
+  "alertLocation": {"SF"},
+  "phoneNumber": {1234567},
+  "email" : {"411411@gmail.com"}
+}
+
+
+@app.route('/api/people/mongodb', methods=['GET'])
+def createUser(userId: str, password: str, nickName: str, alertLocation: str,
+phoneNumber: int, email: str):
+    userId = request.args.get('userId')
+    password = request.args.get('password')
+    nickName = request.args.get('nickName')
+    alertLocation = request.args.get('alertLocation')
+    phoneNumber = request.args.get('phoneNumber')
+    email = request.args.get('email')
+    exist_check = user.find({"$or": [{"email": email}, {"pnumber": phoneNumber}]},{'_id':0}).count()
+    if exist_check >=1:
+        return dumps(print("Account already exsits")),400
+    else:
+        obj_id = user.insert_one(userDocument).insert_id
+        print("Field value is not present")
+        return redirect('http://localhost:3000')
+
+#
+# userDocument = {
+#   "userId": { "%s" },
+#   "password": {"%s"},
+#   "nickName": {"%s"},
+#   "alertLocation": {"%s"},
+#   "phoneNumber": {%},
+#   "email" : {"%s"}
+# } % (userId,password,nickName,alertLocation,phoneNumber,email)
